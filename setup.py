0d30f5cf12caa8f865fcfdc40909e698be2c192f
xiaomingxu1995 <xiaomingxu@tsinghua.edu.cn>
2025-06-27 16:59:33 +0800

    update the version description in setup.py

--- setup.py
+++ setup.py
@@ -181,7 +181,7 @@ ext_modules.append(fused_extension)
 
 setup(
     name='sageattention', 
-    version='2.1.1',  
+    version='2.2.0',  
     author='SageAttention team',
     license='Apache 2.0 License',  
     description='Accurate and efficient plug-and-play low-bit attention.',  
